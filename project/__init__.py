c103ee02732c7f35fc02cb6e5bbe2a20a3d8ee9c
Sagar Panwar <sagarpanwar0123@gmail.com>
2020-10-03 21:25:17 +0530

    removed main from template files

--- project/__init__.py
+++ project/__init__.py
@@ -33,7 +33,7 @@ def create_app():
     app.register_blueprint(auth_blueprint)
 
     # blueprint for non-auth parts of app
-    from .main import main as main_blueprint
-    app.register_blueprint(main_blueprint)
+    from .events import events as events_blueprint
+    app.register_blueprint(events_blueprint)
 
     return app
